d5e98a14c3b771350e272e633d59c4cafd5e883f
Marc <21adresse@gmail.com>
2016-06-19 20:24:35 +0200

    Add files via upload

---
+++ APC_4_ROBOT.ino
@@ -0,0 +1,141 @@
+#include <AFMotor.h>
+#include <Servo.h> 
+#include <NewPing.h>
+#include <Wire.h>
+
+#define MAX_DISTANCE 200 // sets maximum useable sensor measuring distance to 200cm
+#define MAX_SPEED 180 // sets speed of DC traction motors to 180/256 or about 70% of full speed - to get power drain down.
+#define MAX_SPEED_OFFSET 20 // this sets offset to allow for differences between the two DC traction motors
+#define COLL_DIST 10 // sets distance at which robot stops and reverses to 10cm
+#define TURN_DIST COLL_DIST+10 // sets distance at which robot veers away from object (not reverse) to 20cm (10+10)
+
+
+AF_DCMotor motor1(1, MOTOR12_1KHZ); // create motor #1 using M1 output on Motor Drive Shield, set to 1kHz PWM frequency
+AF_DCMotor motor2(4, MOTOR12_1KHZ); // create motor #2, using M2 output, set to 1kHz PWM frequency
+
+
+int pos = 0; // this sets up variables for use in the sketch (code)
+  int maxDist = 0;
+  int maxAngle = 0;
+  int maxRight = 0;
+  int maxLeft = 0;
+  int maxFront = 0;
+int course = 0;
+int curDist = 0;
+String motorSet = "";
+int speedSet = 0;
+char c;
+
+//-------------------------------------------- SETUP LOOP ----------------------------------------------------------------------------
+void setup() {
+  Wire.begin(8);                // join i2c bus with address #8
+  Wire.onReceive(messageRecu); // register event
+  Serial.begin(9600);
+}
+//------------------------------------------------------------------------------------------------------------------------------------
+
+//---------------------------------------------MAIN LOOP ------------------------------------------------------------------------------
+void loop() {
+ 
+   Serial.println ("bonjour");
+  delay (100);
+  
+}
+void messageRecu(int howMany) {
+  while (1 < Wire.available()) { // loop through all but the last
+    char c = Wire.read(); // receive byte as a character
+    Serial.println ("un message est receptionne");
+    Serial.print(c);         // print the character
+  }
+  int x = Wire.read();    // receive byte as an integer
+  Serial.println(x);         // print the integer
+  switch (x)
+  {
+    case 1:
+      {
+        moveForward();
+        delay (100);
+        break;
+      }
+      case 2:
+      {
+        moveBackward();
+        delay (100);
+        break;
+      }
+      case 3:
+      {
+        turnRight();
+        delay (100);
+        break;
+      }
+       case 4:
+      {
+        turnLeft();
+        delay (100);
+        break;
+      }
+      case 5:
+      default:
+      {
+        moveStop();
+        delay (100);
+        break;
+       }
+    
+  }
+}
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+void moveStop() {motor1.run(RELEASE); motor2.run(RELEASE);}  // stop the motors.
+//-------------------------------------------------------------------------------------------------------------------------------------
+void moveForward() {
+    motorSet = "FORWARD";
+    motor1.run(FORWARD);      // turn it on going forward
+    motor2.run(FORWARD);      // turn it on going forward
+  for (speedSet = 0; speedSet < MAX_SPEED; speedSet +=2) // slowly bring the speed up to avoid loading down the batteries too quickly
+  {
+    motor1.setSpeed(speedSet);
+    motor2.setSpeed(speedSet+MAX_SPEED_OFFSET);
+    delay(5);
+  }
+}
+//-------------------------------------------------------------------------------------------------------------------------------------
+void moveBackward() {
+    motorSet = "BACKWARD";
+    motor1.run(BACKWARD);      // turn it on going forward
+    motor2.run(BACKWARD);     // turn it on going forward
+  for (speedSet = 0; speedSet < MAX_SPEED; speedSet +=2) // slowly bring the speed up to avoid loading down the batteries too quickly
+  {
+    motor1.setSpeed(speedSet);
+    motor2.setSpeed(speedSet+MAX_SPEED_OFFSET);
+    delay(5);
+  }
+}  
+//-------------------------------------------------------------------------------------------------------------------------------------
+void turnRight() {
+  motorSet = "RIGHT";
+  motor1.run(FORWARD);      // turn motor 1 forward
+  motor2.run(BACKWARD);     // turn motor 2 backward
+  delay(400); // run motors this way for 400ms
+  motorSet = "FORWARD";
+  motor1.run(FORWARD);      // set both motors back to forward
+  motor2.run(FORWARD);      
+}  
+//-------------------------------------------------------------------------------------------------------------------------------------
+void turnLeft() {
+  motorSet = "LEFT";
+  motor1.run(BACKWARD);     // turn motor 1 backward
+  motor2.run(FORWARD);      // turn motor 2 forward
+  delay(400); // run motors this way for 400ms
+  motorSet = "FORWARD";
+  motor1.run(FORWARD);      // turn it on going forward
+  motor2.run(FORWARD);      // turn it on going forward
+}  
+//-------------------------------------------------------------------------------------------------------------------------------------
+void veerRight() {motor2.run(BACKWARD); delay(400); motor2.run(FORWARD);} // veering right? set right motor backwards for 400ms
+//-------------------------------------------------------------------------------------------------------------------------------------
+void veerLeft() {motor1.run(BACKWARD); delay(400); motor1.run(FORWARD);} // veering left? set left motor backwards for 400ms
+//-------------------------------------------------------------------------------------------------------------------------------------
+
+
---
+++ i2cBluetooth.ino
@@ -0,0 +1,88 @@
+#include <Wire.h>
+
+//#include <SoftwareSerial.h>
+
+//SoftwareSerial mySerial(10, 11);
+const byte TRIGGER_PIN = 4; // Broche TRIGGER
+const byte ECHO_PIN = 3;    // Broche ECHO
+long mm;
+const long MEASURE_TIMEOUT = 25000L; // 40Hz = 25ms = ~8m à 340m/s
+
+static byte x = 0x05;
+static char c = '5';
+
+
+
+void setup() {
+
+  
+  //Serial.begin(9600);
+   
+  /* Initialisation des broches */
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  digitalWrite(TRIGGER_PIN, LOW); // DOIT être à LOW au repos
+  //mySerial.begin(9600);
+  /*communication i2c*/
+  Wire.begin();
+  Serial.begin(9600);
+   
+}
+
+void loop() {
+ 
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN, LOW);
+  // 2) Mesure le temps entre l'envoi de l'impulsion ultrasonique et son écho (si il existe) 
+  long measure = pulseIn(ECHO_PIN, HIGH, MEASURE_TIMEOUT);
+  // 3) Calcul la distance à partir du temps mesuré 
+  mm = measure / 2 * 0.3434;
+  //Serial.println ("loop");
+  
+   if (Serial.available()> 0)
+   // wait for character to arrive
+  {
+     c = Serial.read();
+   
+      if(c =='1') {
+       x = 0x01;   // set the LED on
+      }
+      else if(c == '2'){
+         x = 0x02;
+      }
+      else if (c=='3')
+      {
+         x = 0x03;
+      }
+      else if (c=='4')
+      {
+         x = 0x04;
+      }
+      else if (c == '5')
+      {
+         x = 0x05;
+      }
+     else
+      {
+        x = x;
+      } 
+     }  
+ 
+  
+  
+  Wire.beginTransmission(8);
+  if (mm < 200)
+  {
+    c= '5';
+    x = 0x05;
+  }
+  //Serial.println (x);
+  Wire.write(x);
+  //Serial.println ("message envoye");
+  Wire.endTransmission(); 
+  delay (10);
+  
+}
+
+
